c61496726dcd12872ddacb1ec4bf60c7426e0f70
blackestwhite <pesaregoal@gmail.com>
2023-11-12 13:31:40 +0330

    add basic functionality

---
+++ dockerfile
@@ -0,0 +1,38 @@
+FROM node:18-alpine3.17 as build
+
+# update and install the latest dependencies for the alpine version
+RUN apk update && apk upgrade
+
+# set work dir as app
+WORKDIR /app
+# copy the nuxt project package json and package json lock if available 
+COPY package* ./
+# install all the project npm dependencies
+RUN  npm install
+# copy all other project files to working directory
+COPY . ./
+# build the nuxt project to generate the artifacts in .output directory
+RUN npx nuxt build
+
+# we are using multi stage build process to keep the image size as small as possible
+FROM node:18-alpine3.17
+# update and install latest dependencies, add dumb-init package
+# add a non root user
+RUN apk update && apk upgrade && apk add dumb-init && adduser -D nuxtuser 
+# set non root user
+USER nuxtuser
+
+# set work dir as app
+WORKDIR /app
+# copy the output directory to the /app directory from 
+# build stage with proper permissions for user nuxt user
+COPY --chown=nuxtuser:nuxtuser --from=build /app/.output ./
+# expose 8080 on container
+EXPOSE 3000
+
+# set app host and port . In nuxt 3 this is based on nitro and you can read
+#more on this https://nitro.unjs.io/deploy/node#environment-variables
+ENV HOST=0.0.0.0 PORT=3000 NODE_ENV=production BASE_URL=https://denis.akbari.foundation
+# start the app with dumb init to spawn the Node.js runtime process
+# with signal support
+CMD ["dumb-init","node","/app/server/index.mjs"]
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-    
+  <div class="h-screen flex">
+    <!-- <aside class="h-full border-r w-1/4">
+      <div class="overflow-y-scroll p-4 min-h-full">
+        test
+      </div>
+    </aside> -->
+    <div class="flex flex-col container mx-auto h-full">
+      <div class="p-4 border-b">
+        <p>
+          this is an experimental project built by <a href="https://github.com/blackeswhite" class="text-blue-600">Mahdi Akbari</a>
+        </p>
+        <p>for remote jobs/projects, contact me, <a href="mailto:pesaregoal@gmail.com" class="text-blue-600">pesaregoal@gmail.com</a></p>
+      </div>
+      <div class="grow space-y-2 p-4 overflow-y-scroll">
+          <div v-for="msg in chat">
+              
+              <MDC
+              :value="msg.content"
+              id="mdc"
+              :class="{'bg-blue-100':msg.role=='user', 'border': msg.role=='assistant'}"
+              class="p-3 rounded border"
+              />
+              
+          </div>
+      </div>
+      <div class="p-4 flex">
+        <input v-model="prompt" type="text" class="border p-4 block w-full border-gray-400 rounded focus:border-black transition-all outline-none" placeholder="enter your prompt here" @keydown="sendViaEnter">
+        <button class="p-4 ml-2 border rounded bg-indigo-500 border-indigo-800" @click="sendPrompt">
+          send
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default{
+  data() {
+    return {
+      prompt: '',
+      chat: []
+    }
+  },
+
+  methods: {
+    async sendPrompt() {
+      if (this.prompt == '') {
+        alert('please fill the prompt');
+        return;
+      }
+      this.chat.push({ role: "user", content: this.prompt });
+      this.prompt = ''
+      const response = await $fetch('/api/v1/gen', {method: 'post', body:{content: this.chat}})
+      if (response.ok) {
+        this.chat.push(response.result.choices[0].message)
+      }
+    },
+
+    sendViaEnter(e) {
+      if (e.code != "Enter") return
+      this.sendPrompt()
+    }
+  }
+}
+</script>
+<style>
+code{
+    @apply bg-gray-200 p-1 rounded mx-1
+}
+pre code {
+    @apply block bg-slate-800 text-gray-100 my-2 p-3 rounded
+}
+ul {
+  list-style: disc;
+  @apply px-4
+}
+ol {
+  list-style: decimal;
+  @apply px-4
+}
+</style>
