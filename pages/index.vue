6107ee162cfcad0553ffc2b5912c8084678d0db6
blackestwhite <pesaregoal@gmail.com>
2023-11-12 14:10:43 +0330

    fix typo mistake

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     <div class="flex flex-col container mx-auto h-full">
       <div class="p-4 border-b">
         <p>
-          this is an experimental project built by <a href="https://github.com/blackeswhite" class="text-blue-600">Mahdi Akbari</a>
+          this is an experimental project built by <a href="https://github.com/blackestwhite" class="text-blue-600">Mahdi Akbari</a>
         </p>
         <p>for remote jobs/projects, contact me, <a href="mailto:pesaregoal@gmail.com" class="text-blue-600">pesaregoal@gmail.com</a></p>
       </div>
